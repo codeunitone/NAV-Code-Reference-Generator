33a5cae055424c2356c93d341e35fae6e14cfaf4
BenjaminAlisch <benjamin.alisch@googlemail.com>
2019-10-02 21:22:44 +0200

    using tabs for indentation

--- src/Program.cs
+++ src/Program.cs
@@ -2,11 +2,11 @@
 
 namespace NavCodeReferenceGenerator
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+		class Program
+		{
+				static void Main(string[] args)
+				{
+						Console.WriteLine("Hello World!");
+				}
+		}
 }
