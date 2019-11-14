a40837b2ead1441111f4a22189e3f2bb1fc713c1
BenjaminAlisch <benjamin.alisch@googlemail.com>
2019-11-14 06:37:11 +0100

    Add local vaiable names to procedures

--- src/NAVObjectTypes/Components/Procedure.cs
+++ src/NAVObjectTypes/Components/Procedure.cs
@@ -12,7 +12,20 @@ namespace NavCodeReferenceGenerator
 		
 		public Procedure(string[] content) 
 		{
+			// set name
 			name = ExtractProcedureName(content[0]);
+			
+			// set local variables
+			if (content.Length > 1)
+			{
+				List<Variable> variableList = new List<Variable>();
+				// statrting from 2 because first item is PROCEDURE... and second is always VAR. variables starting on third position
+				for (int i = 2; i < content.Length; i++)
+				{
+					variableList.Add(new Variable(content[i]));
+				}
+				localVariables = variableList;
+			}
 		}
 
 		public string name { get; set; }
--- src/NAVObjectTypes/Components/Variable.cs
+++ src/NAVObjectTypes/Components/Variable.cs
@@ -5,22 +5,21 @@ namespace NavCodeReferenceGenerator
 {
 	public class Variable
 	{
-		public Variable(string name, string datatype)
+		public Variable(string inputString)
 		{
-			this.name = name;
-			this.datatype = datatype;
-		}
-
-		public Variable(string name, string datatype, string source)
-		{
-			this.name = name;
-			this.datatype = datatype;
-			this.source = source;
+			ExtractDetails(inputString);
 		}
 
 		public string name { get; set; }
 		public string datatype { get; set; }
 		public string source { get; set; }
+
+		void ExtractDetails(string inputString)
+		{
+			Console.WriteLine(inputString);
+			// name pos 1 till first @
+			this.name = inputString.Substring(0,inputString.IndexOf("@"));
+		}
 	}
 }
 
